6c4f8273e949623a27b14daee4da7e4a692b4293
Aryan Sharma <122178949+aryansh05@users.noreply.github.com>
2023-03-22 09:36:58 +0530

    Update Leapyear.c

--- Leapyear.c
+++ Leapyear.c
@@ -1,21 +1,25 @@
-//Prgramme to check whether the given year is leap year or not
-#include<stdio.h>
+//Programme to check whether the entered year is leap year or not.
+#include <stdio.h>
 
-int main()
-{
+int main() {
     int year;
-    printf("Enter the number of year: ");
-    scanf("%d",&year);
-    //using IF to check
-    if(year % 4 == 0 && year % 400 == 0 || year % 100 != 0)
-    //if the year is divisble by 4 and divisible by 400 or not divisible by 100 the year is leap year
-    {
-        printf("The given number of year is a Leap year");
-    }
-    else 
-    {
-        printf("The given number of year is not a Leap year");
-    }
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Checking if the year is a leap year or not
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+//if the year is divisble by 4 and then checking if it is divisible by 100 if its is then it will check whether it is divisible by 400 also.
+                printf("%d is a leap year.\n", year);
+            else
+                printf("%d is not a leap year.\n", year);
+        } //this if bracket was for 100 divisibility conditon.
+        else
+            printf("%d is a leap year.\n", year);
+    } else
+        printf("%d is not a leap year.\n", year);
 
     return 0;
 }
